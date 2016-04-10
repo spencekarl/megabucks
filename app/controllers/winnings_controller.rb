c0bd85cdc0fb6c9dfa85b132796c784e5c422e73
Spencer Griffin <spencekarl@gmail.com>
2016-04-10 12:18:46 -0400

    basic validation to check for winners

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,10 +1,15 @@
 class WinningsController < ApplicationController
   def index
+    my_numbers = [8, 14, 16, 33, 38, 48]
+    my_doubler = 2
+    @winners = []
+
     page = "http://www.masslottery.com/data/json/games/lottery/11.json"
     contents = open(page).read
     results = JSON.parse(contents)
 
-    @winning_numbers = results["draws"][0]["winning_num"]
-#    binding.pry
+    @winning_numbers = results["draws"][2]["winning_num"].split("-")
+    @winning_numbers.each { |num| @winners << num if my_numbers.include?(num.to_i) }
+    @did_i_win = (@winners.length < 3) ? "no" : "yes"
   end
 end
