271bea88938c032bbd3fa7930b4c3b310325f2f5
Spencer Griffin <spencekarl@gmail.com>
2016-04-10 11:06:04 -0400

    simple pull of data and display in view

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,4 +1,10 @@
 class WinningsController < ApplicationController
   def index
+    page = "http://www.masslottery.com/data/json/games/lottery/11.json"
+    contents = open(page).read
+    results = JSON.parse(contents)
+
+    @winning_numbers = results["draws"][0]["winning_num"]
+#    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'winnings/index'
+  root 'winnings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
