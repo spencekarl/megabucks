fa9dabe1cf7313ce60799e93a47669acb5f20358
Spencer Griffin <spencekarl@gmail.com>
2016-04-10 13:57:13 -0400

    refactoring

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,30 +1,34 @@
 class WinningsController < ApplicationController
   def index
-    my_numbers = [1, 11, 12, 16, 23, 28]
+    my_numbers = [1, 11, 16, 33, 38, 48]
     my_doubler = 6
-    @winning_count = 0
-    prizes = {3 => 2,
-              4 => 100,
-              5 => 2500,
-              6 => 500000}
+    winning_count = 0
+    prizes = { 3 => 2,
+               4 => 100,
+               5 => 2500,
+               6 => 500000 }
+    phrase = { :winner => ["win", "winner", "winning"],
+               :loser  => ["lose", "loser", "losing"] }
 
     # open json lottery data page and parse results into ruby array
     page = "http://www.masslottery.com/data/json/games/lottery/11.json"
     contents = open(page).read
     results = JSON.parse(contents)
 
-    # store winning numbers, jackport, bonus
+    # store winning numbers, jackpot, bonus
     @winning_numbers = results["draws"][0]["winning_num"].split("-")
     prizes[6] = results["draws"][0]["jackpot"]
-    bonus = results["draws"][0]["bonus"]
+    @bonus_number = results["draws"][0]["bonus"]
 
     # determine if i won
-    @winning_numbers.each { |num| @winning_count += 1 if my_numbers.include?(num.to_i) }
-    @i_won = (@winning_count < 3) ? "no" : "yes"
-    bonus_multiplier = (bonus.to_i == my_doubler && @winning_count < 6) ? 2 : 1
-    
+    @winning_numbers.each { |num| winning_count += 1 if my_numbers.include?(num.to_i) }
+    @i_won = (winning_count >= 3) ? :winner : :loser
+    bonus_multiplier = (@bonus_number.to_i == my_doubler && winning_count < 6) ? 2 : 1
+
+    # determine winning/losing phrase
+    @announcement = phrase[@i_won].sample
     # determine winning amount
-    @winnings = prizes[@winning_count] * bonus_multiplier if @i_won
+    @winnings = prizes[winning_count] * bonus_multiplier if @i_won
 
   end
 end
