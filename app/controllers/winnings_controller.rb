9e1b41d3518808430b4bc423b3c86e2bbc9b8bca
Spencer Griffin <spencekarl@gmail.com>
2016-04-10 13:09:40 -0400

    added validation for prize winnings and bonus

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,15 +1,30 @@
 class WinningsController < ApplicationController
   def index
-    my_numbers = [8, 14, 16, 33, 38, 48]
-    my_doubler = 2
-    @winners = []
+    my_numbers = [1, 11, 12, 16, 23, 28]
+    my_doubler = 6
+    @winning_count = 0
+    prizes = {3 => 2,
+              4 => 100,
+              5 => 2500,
+              6 => 500000}
 
+    # open json lottery data page and parse results into ruby array
     page = "http://www.masslottery.com/data/json/games/lottery/11.json"
     contents = open(page).read
     results = JSON.parse(contents)
 
-    @winning_numbers = results["draws"][2]["winning_num"].split("-")
-    @winning_numbers.each { |num| @winners << num if my_numbers.include?(num.to_i) }
-    @did_i_win = (@winners.length < 3) ? "no" : "yes"
+    # store winning numbers, jackport, bonus
+    @winning_numbers = results["draws"][0]["winning_num"].split("-")
+    prizes[6] = results["draws"][0]["jackpot"]
+    bonus = results["draws"][0]["bonus"]
+
+    # determine if i won
+    @winning_numbers.each { |num| @winning_count += 1 if my_numbers.include?(num.to_i) }
+    @i_won = (@winning_count < 3) ? "no" : "yes"
+    bonus_multiplier = (bonus.to_i == my_doubler && @winning_count < 6) ? 2 : 1
+    
+    # determine winning amount
+    @winnings = prizes[@winning_count] * bonus_multiplier if @i_won
+
   end
 end
