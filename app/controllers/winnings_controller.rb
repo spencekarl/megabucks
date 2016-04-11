42ab440f50f8eeb29c6897448f23d53f3e1f5c02
Spencer Griffin <spencekarl@gmail.com>
2016-04-11 11:35:47 -0400

    added more phrases and reorganized code a little

--- app/controllers/winnings_controller.rb
+++ app/controllers/winnings_controller.rb
@@ -1,46 +1,47 @@
 class WinningsController < ApplicationController
   def index
-    my_numbers = [8, 14, 16, 33, 38, 48]
+    #my_numbers = [8, 14, 16, 33, 38, 48]
+    my_numbers = [1, 11, 16, 29, 12, 50]
     my_doubler = 2
     winning_count = 0
-    prizes = { 3 => 2,
-               4 => 100,
-               5 => 2500,
-               6 => 500000 }
+    prizes = [0, 0, 0, 2, 100, 2500, 500000]
     phrases = { :winner => ["holy sh!t, you won!!",
                             "you're a winner harry",
                             "wtf, you actually won!",
                             "hell yeah!!!!",
-                            "finally :)"],
-                :loser  => ["another week, another loser",
+                            "finally :)",
+                            "you did it!"],
+                :loser  => ["another week, another loss",
                             "did you really think the odds were in your favor?",
                             "you're a loser, again",
                             "maybe next week :-(",
-                            "you won...$0",
-                            "$zero"] }
+                            "you get nothing!",
+                            "i guess your numbers kinda suck"] }
+    week = 0
 
-    # open json lottery data page and parse results into ruby array
+    # open json lottery data page and parse data into ruby array
     page = "http://www.masslottery.com/data/json/games/lottery/11.json"
     contents = open(page).read
-    results = JSON.parse(contents)
+    data = JSON.parse(contents)
 
-    # store winning numbers, jackpot, bonus, and weekly video
-    @winning_numbers = results["draws"][0]["winning_num"].split("-")
-    prizes[6] = results["draws"][0]["jackpot"].delete "$"
-    @bonus_number = results["draws"][0]["bonus"]
-    @video_id = results["draws"][0]["video_id"]
-    @video_url = "//www.youtube.com/embed/" + @video_id + "?autoplay=1&showinfo=0&autohide=1&controls=0&playlist=skf8mvqFqSo&loop=1"
+    # store winning numbers, bonus, jackpot
+    winning_numbers = data["draws"][week]["winning_num"].split("-")
+    bonus_number = data["draws"][week]["bonus"]
+    prizes[6] = data["draws"][week]["jackpot"].delete "$"
+    # build video url
+    video_id = data["draws"][week]["video_id"]
+    youtube_params = "?autoplay=1&showinfo=0&autohide=1&controls=0&playlist=skf8mvqFqSo&loop=1"
+    @video_url = "//www.youtube.com/embed/" + video_id + youtube_params
 
     # determine if i won
-    @winning_numbers.each { |num| winning_count += 1 if my_numbers.include?(num.to_i) }
-    @i_won = (winning_count >= 3) ? :winner : :loser
-    bonus_multiplier = (@bonus_number.to_i == my_doubler && winning_count < 6) ? 2 : 1
+    winning_numbers.each { |num| winning_count += 1 if my_numbers.include?(num.to_i) }
+    @i_won = (prizes[winning_count] > 0) ? :winner : :loser
+    bonus_multiplier = (bonus_number.to_i == my_doubler && winning_count < 6) ? 2 : 1
 
     # determine winning/losing phrase
     @announcement = phrases[@i_won].sample.upcase
 
     # determine winning amount
     @winnings = prizes[winning_count] * bonus_multiplier if @i_won == :winner
-
   end
 end
